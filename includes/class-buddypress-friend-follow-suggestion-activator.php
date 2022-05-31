fa849430dd94bca4d0bf494424f0dc99cf73931e
Wbcom Dev <pallavi@wbcomdesigns.com>
2022-05-31 12:32:14 +0530

    Added default option

--- includes/class-buddypress-friend-follow-suggestion-activator.php
+++ includes/class-buddypress-friend-follow-suggestion-activator.php
@@ -29,7 +29,14 @@ class Buddypress_Friend_Follow_Suggestion_Activator {
 	 * @since    1.0.0
 	 */
 	public static function activate() {
-
+		$bffs_general_setting = get_option( 'bffs_general_setting' );
+		if ( false === $bffs_general_setting ) {
+			$bffs_general_setting = array(
+				'enable_profile_match' => 1,
+				'profile_st_percentage' => 20,
+			);
+			update_option( 'bffs_general_setting', $bffs_general_setting );
+		}
 	}
 
 }
