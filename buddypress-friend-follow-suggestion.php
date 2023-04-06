89b9be3bdc60c9c93add77ca136dba185eb695c4
Varun Kumar Dubey <vapvarun@gmail.com>
2023-04-07 00:30:35 +0530

    Update buddypress-friend-follow-suggestion.php

--- buddypress-friend-follow-suggestion.php
+++ buddypress-friend-follow-suggestion.php
@@ -15,7 +15,7 @@
  * Plugin Name:       BuddyPress Friend & Follow Suggestion
  * Plugin URI:        https://wbcomdesigns.com/downloads/buddypress-friend-follow-suggestion
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.4.3
+ * Version:           1.4.4
  * Author:            Wbcom Designs
  * Author URI:        https://wbcomdesigns.com/
  * License:           GPL-2.0+
@@ -34,7 +34,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'BUDDYPRESS_FRIEND_FOLLOW_SUGGESTION_VERSION', '1.4.3' );
+define( 'BUDDYPRESS_FRIEND_FOLLOW_SUGGESTION_VERSION', '1.4.4' );
 
 
 if ( ! defined( 'BFFS_PLUGIN_VERSION' ) ) {
