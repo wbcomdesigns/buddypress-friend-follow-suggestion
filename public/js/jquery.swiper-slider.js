11e350add67489d5473a59d1b65563fbfb5ca350
Dishant Patel <dishant.patel1007@gmail.com>
2024-08-29 18:52:18 +0530

    Update js library

--- public/js/jquery.swiper-slider.js
+++ public/js/jquery.swiper-slider.js
@@ -8,192 +8,179 @@
  * https://github.com/do-web/jTinder/blob/master/LICENSE
  */
 ;(function ($, window, document, undefined) {
-	var pluginName = "jTinder",
-		defaults = {
-			onDislike: null,
-			onLike: null,
-			animationRevertSpeed: 200,
-			animationSpeed: 400,
-			threshold: 1,
-			likeSelector: '.like',
-			dislikeSelector: '.dislike'
-		};
+    var pluginName = "jTinder",
+        defaults = {
+            onDislike: null,
+            onLike: null,
+            animationRevertSpeed: 200,
+            animationSpeed: 400,
+            threshold: 1,
+            likeSelector: '.like',
+            dislikeSelector: '.dislike'
+        };
 
-	var container = null;
-	var panes = null;
-	var $that = null;
-	var xStart = 0;
-	var yStart = 0;
-	var touchStart = false;
-	var posX = 0, posY = 0, lastPosX = 0, lastPosY = 0, pane_width = 0, pane_count = 0, current_pane = 0;
+    function Plugin(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
 
-	function Plugin(element, options) {
-		this.element = element;
-		this.settings = $.extend({}, defaults, options);
-		this._defaults = defaults;
-		this._name = pluginName;
-		this.init(element);
-	}
+        // Instance-specific variables
+        this.container = null;
+        this.panes = null;
+        this.xStart = 0;
+        this.yStart = 0;
+        this.touchStart = false;
+        this.posX = 0;
+        this.posY = 0;
+        this.lastPosX = 0;
+        this.lastPosY = 0;
+        this.pane_width = 0;
+        this.pane_count = 0;
+        this.current_pane = 0;
 
-	Plugin.prototype = {
+        this.init();
+    }
 
+    Plugin.prototype = {
+        init: function () {
+            this.container = $(">ul", this.element);
+            this.panes = $(">ul>li", this.element);
+            this.pane_width = this.container.width();
+            this.pane_count = this.panes.length;
+            this.current_pane = this.pane_count - 1;
 
-		init: function (element) {
+            $(this.element).bind('touchstart mousedown', this.handler.bind(this));
+            $("body").bind('touchmove mousemove', this.handler.bind(this));
+            $("body").bind('touchend mouseup', this.handler.bind(this));
+        },
 
-			container = $(">ul", element);
-			panes = $(">ul>li", element);
-			pane_width = container.width();
-			pane_count = panes.length;
-			current_pane = panes.length - 1;
-			$that = this;
-			
-			$(element).bind('touchstart mousedown', this.handler);
-			//$(element).bind('touchmove mousemove', this.handler);
-			//$(element).bind('touchend mouseup', this.handler);			
-			$('body').bind('touchmove mousemove', this.handler);
-			$('body').bind('touchend mouseup', this.handler);
-		},
+        showPane: function (index) {
+            this.panes.eq(this.current_pane).hide();
+            this.current_pane = index;
+        },
 
-		showPane: function (index) {
-			panes.eq(current_pane).hide();
-			current_pane = index;
-		},
+        next: function () {
+            if(this.current_pane !== 1) {
+                return this.showPane(this.current_pane - 1);
+            }
+        },
 
-		next: function () {
-			if(current_pane !== 1){
-				return this.showPane(current_pane - 1);
-			}
-		},
+        dislike: function() {
+            var self = this;
+            this.panes.eq(this.current_pane).animate({"transform": "translate(-" + (this.pane_width) + "px," + (this.pane_width * -1.5) + "px) rotate(-60deg)"}, this.settings.animationSpeed, function () {
+                if(self.settings.onDislike) {
+                    self.settings.onDislike(self.panes.eq(self.current_pane));
+                }
+                self.next();
+            });
+        },
 
-		dislike: function() {
-			panes.eq(current_pane).animate({"transform": "translate(-" + (pane_width) + "px," + (pane_width*-1.5) + "px) rotate(-60deg)"}, $that.settings.animationSpeed, function () {
-				if($that.settings.onDislike) {
-					$that.settings.onDislike(panes.eq(current_pane));
-				}
-				$that.next();
-			});
-		},
+        like: function() {
+            var self = this;
+            this.panes.eq(this.current_pane).animate({"transform": "translate(" + (this.pane_width) + "px," + (this.pane_width * -1.5) + "px) rotate(60deg)"}, this.settings.animationSpeed, function () {
+                if(self.settings.onLike) {
+                    self.settings.onLike(self.panes.eq(self.current_pane));
+                }
+                self.next();
+            });
+        },
 
-		like: function() {
-			panes.eq(current_pane).animate({"transform": "translate(" + (pane_width) + "px," + (pane_width*-1.5) + "px) rotate(60deg)"}, $that.settings.animationSpeed, function () {
-				if($that.settings.onLike) {
-					$that.settings.onLike(panes.eq(current_pane));
-				}
-				$that.next();
-			});
-		},
+        handler: function (ev) {
+            ev.preventDefault();
 
-		handler: function (ev) {
-			ev.preventDefault();
+            switch (ev.type) {
+                case 'touchstart':
+                case 'mousedown':
+                    if(this.touchStart === false) {
+                        this.touchStart = true;
+                        this.xStart = (typeof ev.pageX == 'undefined') ? ev.originalEvent.touches[0].pageX : ev.pageX;
+                        this.yStart = (typeof ev.pageY == 'undefined') ? ev.originalEvent.touches[0].pageY : ev.pageY;
+                    }
+                    break;
 
-			switch (ev.type) {
-				case 'touchstart':
-					if(touchStart === false) {
-						touchStart = true;
-						xStart = ev.originalEvent.touches[0].pageX;
-						yStart = ev.originalEvent.touches[0].pageY;
-					}
-				case 'mousedown':
-					if(touchStart === false) {
-						touchStart = true;
-						xStart = ev.pageX;
-						yStart = ev.pageY;
-					}
-				case 'mousemove':
-				case 'touchmove':
-					if(touchStart === true) {
-						var pageX = typeof ev.pageX == 'undefined' ? ev.originalEvent.touches[0].pageX : ev.pageX;
-						var pageY = typeof ev.pageY == 'undefined' ? ev.originalEvent.touches[0].pageY : ev.pageY;
-						var deltaX = parseInt(pageX) - parseInt(xStart);
-						var deltaY = parseInt(pageY) - parseInt(yStart);
-						var percent = ((100 / pane_width) * deltaX) / pane_count;
-						posX = deltaX + lastPosX;
-						posY = deltaY + lastPosY;
+                case 'mousemove':
+                case 'touchmove':
+                    if(this.touchStart === true) {
+                        var pageX = (typeof ev.pageX == 'undefined') ? ev.originalEvent.touches[0].pageX : ev.pageX;
+                        var pageY = (typeof ev.pageY == 'undefined') ? ev.originalEvent.touches[0].pageY : ev.pageY;
+                        var deltaX = parseInt(pageX) - parseInt(this.xStart);
+                        var deltaY = parseInt(pageY) - parseInt(this.yStart);
+                        var percent = ((100 / this.pane_width) * deltaX) / this.pane_count;
 
-						panes.eq(current_pane).css("transform", "translate(" + posX + "px," + posY + "px) rotate(" + (percent / 2) + "deg)");
+                        this.posX = deltaX + this.lastPosX;
+                        this.posY = deltaY + this.lastPosY;
 
-						var opa = (Math.abs(deltaX) / $that.settings.threshold) / 100 + 0.2;
-						if(opa > 1.0) {
-							opa = 1.0;
-						}
-						if (posX >= 0) {
-							panes.eq(current_pane).find($that.settings.likeSelector).css('opacity', opa);
-							panes.eq(current_pane).find($that.settings.dislikeSelector).css('opacity', 0);
-							panes.eq(current_pane).find($that.settings.dislikeSelector).removeClass('dislike-active');
-							if ( opa > 0 ) {
-									panes.eq(current_pane).find($that.settings.likeSelector).addClass('like-active');
-							} else {
-								panes.eq(current_pane).find($that.settings.likeSelector).removeClass('like-active');
-							}
-						} else if (posX < 0) {
-							panes.eq(current_pane).find($that.settings.dislikeSelector).css('opacity', opa);
-							panes.eq(current_pane).find($that.settings.likeSelector).css('opacity', 0);
-							panes.eq(current_pane).find($that.settings.likeSelector).removeClass('like-active');
-							if ( opa > 0 ) {
-									panes.eq(current_pane).find($that.settings.dislikeSelector).addClass('dislike-active');
-							} else {
-								panes.eq(current_pane).find($that.settings.dislikeSelector).removeClass('dislike-active');
-							}
-						}						
-					}
-					break;
-				case 'mouseup':
-				case 'touchend':
+                        this.panes.eq(this.current_pane).css("transform", "translate(" + this.posX + "px," + this.posY + "px) rotate(" + (percent / 2) + "deg)");
 
-					if (touchStart == true) {
-						var pageX = (typeof ev.pageX == 'undefined') ? ev.originalEvent.changedTouches[0].pageX : ev.pageX;
-						var pageY = (typeof ev.pageY == 'undefined') ? ev.originalEvent.changedTouches[0].pageY : ev.pageY;
-						var deltaX = parseInt(pageX) - parseInt(xStart);
-						var deltaY = parseInt(pageY) - parseInt(yStart);
+                        var opa = (Math.abs(deltaX) / this.settings.threshold) / 100 + 0.2;
+                        if(opa > 1.0) {
+                            opa = 1.0;
+                        }
+                        if (this.posX >= 0) {
+                            this.panes.eq(this.current_pane).find(this.settings.likeSelector).css('opacity', opa);
+                            this.panes.eq(this.current_pane).find(this.settings.dislikeSelector).css('opacity', 0);
+                        } else if (this.posX < 0) {
+                            this.panes.eq(this.current_pane).find(this.settings.dislikeSelector).css('opacity', opa);
+                            this.panes.eq(this.current_pane).find(this.settings.likeSelector).css('opacity', 0);
+                        }
+                    }
+                    break;
 
-						posX = deltaX + lastPosX;
-						posY = deltaY + lastPosY;
-						var opa = Math.abs((Math.abs(deltaX) / $that.settings.threshold) / 100 + 0.2);
+                case 'mouseup':
+                case 'touchend':
+                    var self = this;
+                    if (this.touchStart == true) {
+                        var pageX = (typeof ev.pageX == 'undefined') ? ev.originalEvent.changedTouches[0].pageX : ev.pageX;
+                        var pageY = (typeof ev.pageY == 'undefined') ? ev.originalEvent.changedTouches[0].pageY : ev.pageY;
+                        var deltaX = parseInt(pageX) - parseInt(this.xStart);
+                        var deltaY = parseInt(pageY) - parseInt(this.yStart);
 
-						if (opa >= 1) {
-							if (posX > 0) {
-								panes.eq(current_pane).animate({"transform": "translate(" + (pane_width) + "px," + (posY + pane_width) + "px) rotate(60deg)"}, $that.settings.animationSpeed, function () {
-									if($that.settings.onLike) {
-										$that.settings.onLike(panes.eq(current_pane));
-									}
-									$that.next();
-								});
-							} else {
-								panes.eq(current_pane).animate({"transform": "translate(-" + (pane_width) + "px," + (posY + pane_width) + "px) rotate(-60deg)"}, $that.settings.animationSpeed, function () {
-									if($that.settings.onDislike) {
-										$that.settings.onDislike(panes.eq(current_pane));
-									}
-									$that.next();
-								});
-							}
-						} else {
-							lastPosX = 0;
-							lastPosY = 0;
-							panes.eq(current_pane).animate({"transform": "translate(0px,0px) rotate(0deg)"}, $that.settings.animationRevertSpeed);
-							panes.eq(current_pane).find($that.settings.likeSelector).animate({"opacity": 0}, $that.settings.animationRevertSpeed);
-							panes.eq(current_pane).find($that.settings.dislikeSelector).animate({"opacity": 0}, $that.settings.animationRevertSpeed);
-							
-							panes.eq(current_pane).find($that.settings.likeSelector).removeClass('like-active');
-							panes.eq(current_pane).find($that.settings.dislikeSelector).removeClass('dislike-active');
-						}
-						touchStart = false;
-					}
-					break;
-			}
-		}
-	};
+                        this.posX = deltaX + this.lastPosX;
+                        this.posY = deltaY + this.lastPosY;
+                        var opa = Math.abs((Math.abs(deltaX) / this.settings.threshold) / 100 + 0.2);
 
-	$.fn[ pluginName ] = function (options) {
-		this.each(function () {
-			if (!$.data(this, "plugin_" + pluginName)) {
-				$.data(this, "plugin_" + pluginName, new Plugin(this, options));
-			}
-			else if ($.isFunction(Plugin.prototype[options])) {
-				$.data(this, 'plugin_' + pluginName)[options]();
-		    }
-		});
+                        if (opa >= 1) {
+                            if (this.posX > 0) {
+                                this.panes.eq(this.current_pane).animate({"transform": "translate(" + (this.pane_width) + "px," + (this.posY + this.pane_width) + "px) rotate(60deg)"}, this.settings.animationSpeed, function () {
+                                    if(self.settings.onLike) {
+                                        self.settings.onLike(self.panes.eq(self.current_pane));
+                                    }
+                                    self.next();
+                                });
+                            } else {
+                                this.panes.eq(this.current_pane).animate({"transform": "translate(-" + (this.pane_width) + "px," + (this.posY + this.pane_width) + "px) rotate(-60deg)"}, this.settings.animationSpeed, function () {
+                                    if(self.settings.onDislike) {
+                                        self.settings.onDislike(self.panes.eq(self.current_pane));
+                                    }
+                                    self.next();
+                                });
+                            }
+                        } else {
+                            this.lastPosX = 0;
+                            this.lastPosY = 0;
+                            this.panes.eq(this.current_pane).animate({"transform": "translate(0px,0px) rotate(0deg)"}, this.settings.animationRevertSpeed);
+                            this.panes.eq(this.current_pane).find(this.settings.likeSelector).animate({"opacity": 0}, this.settings.animationRevertSpeed);
+                            this.panes.eq(this.current_pane).find(this.settings.dislikeSelector).animate({"opacity": 0}, this.settings.animationRevertSpeed);
+                        }
+                        this.touchStart = false;
+                    }
+                    break;
+            }
+        }
+    };
 
-		return this;
-	};
+    $.fn[pluginName] = function (options) {
+        this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            }
+            else if ($.isFunction(Plugin.prototype[options])) {
+                $.data(this, 'plugin_' + pluginName)[options]();
+            }
+        });
+
+        return this;
+    };
 
 })(jQuery, window, document);
